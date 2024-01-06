2e5a9108dbc9d86406b9cb475a3347b28333d263
Aneel Singh <ajsingh1012@gmail.com>
2024-01-06 21:48:33 +0000

    added firebase capability

--- pages/chats/[id].js
+++ pages/chats/[id].js
@@ -1,6 +1,28 @@
 import { useRouter } from 'next/router'
+import { app, database } from '../../app/firebase/config';
+import { collection, addDoc } from 'firebase/firestore';
+
+const dbInstance = collection(database, 'notes');
  
 export default function Page() {
   const router = useRouter()
-  return <p>Post: {router.query.id}</p>
+  const saveNote = () => {
+    addDoc(dbInstance, {
+        noteTitle: "moon",
+        noteDesc: "world"
+    }).then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+        console.log('note saved')
+    })
+  }
+  saveNote()
+  console.log('hello')
+  return (
+    <div>
+      <p>Post: {router.query.id}</p>
+      <button onClick={saveNote}>
+        Save Note
+      </button>
+    </div>
+  )
 }
