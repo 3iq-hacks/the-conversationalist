5b08643291cb4387e9e9b18d0bae8ff368ff1174
Joshua Ji <joshua.ji@jiwhiz.com>
2024-01-06 20:30:40 +0000

    Home page shenanigans

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,27 @@
 import Image from 'next/image'
+import { Card, Button, Badge } from 'flowbite-react'
+import HomepageCard from '@/components/HomepageCard'
 
 export default function Home() {
   return (
-    <main>
-      <section className="bg-white dark:bg-gray-800">
-        <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Revive the dead</h1>
-            <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Or talk to anime girls. We don&apos;t discriminate.</p>
-            <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-                <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                    Get started
-                    <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
-                </a>
-                <a href="#" className="inline-flex justify-center items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                    Learn more
-                </a>  
-            </div>
-        </div>
-      </section>
+    <main className="bg-white dark:bg-gray-800 min-h-screen flex flex-col">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Revive the dead</h1>
+        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Or talk to anime girls. We don&apos;t discriminate.</p>
+      </div>
+      <div className='flex flex-row h-full justify-evenly'>
+        <HomepageCard imgSrc='/imgs/trump.jpg' imgAlt='trump' title='Donald Trump' description='Tell him how you really think about him!' />
+        <HomepageCard imgSrc='/imgs/cat.jpg' imgAlt='anime catgirl' title='Anime Catgirl' description="You'll forget you're talking to an AI!" />
+        <Card className="w-[350px] text-center">
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Create your own!
+          </h5>
+          <span className="flex flex-row justify-center">
+            <Badge className="ml-2">Coming soon!!</Badge>
+            <Image src="/imgs/kappa.png" width={20} height={20} alt="hehe" />
+          </span>
+        </Card>
+      </div>
     </main>
   )
 }
---
+++ components/HomepageCard.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import { Card, Button, Badge } from 'flowbite-react'
+
+interface Props {
+    imgSrc: string
+    imgAlt: string
+    title: string
+    description: string
+}
+
+export default function Component(props: Props) {
+    return (
+        <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 text-center">
+            <Image src={props.imgSrc} width={350} height={350} alt={props.imgAlt} objectFit='contain' objectPosition='top' />
+            <div className="p-5 flex flex-col space-y-2">
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {props.title}
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                    {props.description}
+                </p>
+                <Button>
+                    Start a conversation
+                    <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            fillRule="evenodd"
+                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+                </Button>
+            </div>
+        </div>
+    )
+}
