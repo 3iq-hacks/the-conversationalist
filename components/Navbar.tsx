0d5c4b4be344acf98b07d0ba2d94748b8be6c5e5
Joshua Ji <joshua.ji@jiwhiz.com>
2024-01-06 19:36:12 +0000

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,9 +7,9 @@ import { Navbar, DarkThemeToggle } from 'flowbite-react';
 export default function Component() {
   return (
     <Navbar fluid>
-      <Navbar.Brand as={Link} href="https://flowbite-react.com" className="">
+      <Navbar.Brand as={Link} href="https://github.com/3iq-hacks">
         <Image src="/cat.webp" width={24} height={24} alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white pl-2">A 3IQ Hacks Creation</span>
+        <span className="self-center whitespace-nowrap text-xl font-semibold text-black dark:text-white pl-2">A 3IQ Hacks Creation</span>
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
