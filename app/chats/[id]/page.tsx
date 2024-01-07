51375fd6c9812184f84d00ffd797e08f2e4b0f53
Joshua Ji <joshua.ji@jiwhiz.com>
2024-01-07 18:47:39 +0000

    get it wokring

--- app/chats/[id]/page.tsx
+++ app/chats/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { database } from '@/lib/firebase/config';
-import { collection, Timestamp, query, onSnapshot, getDocs, orderBy, addDoc, doc } from 'firebase/firestore';
+import { collection, Timestamp, query, onSnapshot, getDocs, orderBy, addDoc, doc, getDoc, limit } from 'firebase/firestore';
 import TextStrip from '@/components/TextStrip'
 import { useState, useEffect } from 'react';
 import TextBubble, { Props as TextBubbleProps } from '@/components/TextBubble'
@@ -12,6 +12,7 @@ import { Button } from 'flowbite-react';
 var temp: any;
 
 export default function Page({ params }: { params: { id: string } }) {
+  const chatDocRef = doc(database, 'chats', params.id);
   const dbInstance = collection(database, 'chats/' + params.id + '/messages');
 
   const [notesArray, setNotesArray] = useState<TextBubbleProps[]>([]);
@@ -43,20 +44,64 @@ export default function Page({ params }: { params: { id: string } }) {
     temp = childData.target
   }
   //const router = useRouter()
-  const saveNote = () => {
-    addDoc(dbInstance, {
+  const saveNote = async () => {
+    const docRef = await addDoc(dbInstance, {
       textContent: textInput,
       textTime: Timestamp.now(),
       textAuthor: "user"
     })
-      .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
-        console.log('note saved')
-        temp.value = '';
+    temp.value = '';
+
+    console.log("Document written with ID: ", docRef.id);
+    console.log('note saved');
+    getNotes();
+
+    // now, send a request to api/note.ts 
+    // to generate a response
+
+    const docSnap = await getDoc(chatDocRef);
+    const currChatData = docSnap.data();
+    console.log('currChatData: ', currChatData)
+    if (!currChatData) {
+      console.error('currChatData is null')
+      return;
+    }
+
+    // get 5 most recent messages
+    const messagesQuery = query(dbInstance, orderBy('textTime', 'desc'), limit(5));
+    const messagesDocs = await getDocs(messagesQuery);
+    const messagesArray: any[] = [];
+    messagesDocs.forEach((doc) => {
+      const data = doc.data();
+      messagesArray.push({
+        role: data.textAuthor,
+        content: data.textContent
+      });
+    })
+    const response = await fetch('/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        model: currChatData.model,
+        initialPrompt: currChatData.initialPrompt,
+        messages: messagesArray
       })
-      .finally(() => {
-        getNotes();
-      })
+    });
+    // get response body
+    const responseJson = await response.json();
+    console.log('responseJson: ', responseJson);
+
+    // save responseJson to firebase 
+    // it is of the form { role, content }
+    const responseDocRef = await addDoc(dbInstance, {
+      textContent: responseJson.content,
+      textTime: Timestamp.now(),
+      textAuthor: responseJson.role
+    })
+    console.log('responseDocRef: ', responseDocRef)
+    getNotes();
   }
 
   return (
--- app/new/[person]/page.tsx
+++ app/new/[person]/page.tsx
@@ -16,11 +16,12 @@ const getGPTModel = (name: string) => {
     }
 }
 
+
 const getInitialPrompt = (name: string) => {
     if (name == 'trump') {
-        return 'You are pretending to be Trump, and it is terrifyingly scary.'
+        return { 'role': 'system', 'content': 'You are pretending to be Trump, and it is terrifyingly scary. The user you are talking to is unknown.' }
     } else {
-        return 'You are an Anime Waifu, you are obsessed and in love with the computer science student.'
+        return { 'role': 'system', 'content': 'You are an Anime Waifu, you are obsessed and in love with the computer science student.' }
     }
 }
 
@@ -39,16 +40,9 @@ export default function Page({ params }: { params: { person: string } }) {
                 const chatDocRef = await addDoc(chats, {
                     createdAt: Timestamp.now(),
                     model: getGPTModel(params.person),
+                    initialPrompt: getInitialPrompt(params.person)
                 });
                 console.log('Created new chat document: ', chatDocRef.id)
-                // add a collection into charDocRef. This is the messages into the chat
-                const messagesCollection = collection(chatDocRef, 'messages');
-                const messagesDocRef = await addDoc(messagesCollection, {
-                    textTime: Timestamp.now(),
-                    textContent: getInitialPrompt(params.person),
-                    textAuthor: 'server'
-                });
-                console.log('Created new message document: ', messagesDocRef.id)
 
                 // redirect to /chats/:id
                 console.log(`Redirecting to /chats/${chatDocRef.id}`)
