8f84368f2517d71d7907c8493465770a4d44c88d
Joshua Ji <joshua.ji@jiwhiz.com>
2024-01-07 03:41:30 +0000

    Clean up image component

--- components/HomepageCard.tsx
+++ components/HomepageCard.tsx
@@ -12,7 +12,7 @@ interface Props {
 export default function Component(props: Props) {
     return (
         <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 text-center">
-            <Image src={props.imgSrc} width={350} height={350} alt={props.imgAlt} objectFit='contain' objectPosition='top' />
+            <Image src={props.imgSrc} width={350} height={350} alt={props.imgAlt} />
             <div className="p-5 flex flex-col">
                 <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                     {props.title}
