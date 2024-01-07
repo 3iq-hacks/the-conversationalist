7d9f8c57b6153dc206208fe1da17f9909ef424a4
Joshua Ji <joshua.ji@jiwhiz.com>
2024-01-07 18:52:18 +0000

    shift right for users

--- components/TextBubble.tsx
+++ components/TextBubble.tsx
@@ -13,10 +13,11 @@ function datify(time: Timestamp) {
 }
 
 export default function Component(props: Props) {
+   const extraClass = props.author == "user" ? "justify-end" : "justify-start"
    return (
-      <div className="flex items-start gap-2.5">
+      <div className={`flex items-start gap-2.5 w-full ${extraClass} `}>
          <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
-            <div className="flex items-center space-x-2 rtl:space-x-reverse">
+            <div className="flex items-center space-x-2 rtl:space-x-reverse" >
                <span className="text-sm font-semibold text-gray-900 dark:text-white">{props.author}</span>
                <span className="text-sm font-normal text-gray-500 dark:text-gray-400">{datify(props.time)}</span>
             </div>
