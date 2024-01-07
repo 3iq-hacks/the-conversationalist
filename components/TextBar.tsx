7730ec1da2b8a2af5010dd8e999a2adab30b13fa
Aneeljyot Alagh <69876558+ajsingh1012@users.noreply.github.com>
2024-01-07 07:04:54 +0000

    minor css change

--- components/TextBar.tsx
+++ components/TextBar.tsx
@@ -7,7 +7,7 @@ export default function Component({ childToParent }: any) {
         type="text"
         name="textInput"
         onInput={i => childToParent(i)}
-        style={{width: "80vw"}}
+        style={{width: "80vw", marginRight: '5px'}}
         className="h-full max-w-full bg-gray-50 h-auto border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       ></input>
     </div>
